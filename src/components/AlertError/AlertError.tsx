a9818b2c28752bbf0df2b3ba5f6c62340edbb167
JeanCsantos7 <jean.carlos87678@gmail.com>
2024-12-18 22:40:49 -0300

    create deploy frontend

--- src/components/AlertError/AlertError.tsx
+++ src/components/AlertError/AlertError.tsx
@@ -4,7 +4,7 @@ export default function AlertError() {
     return (
         <Alert className=" flex m-auto justify-center items-center transition-[ease-in-out] w-[78%] h-[4.5vh] mb-[8%] p-[3%] bg-[#fa3e3e] opacity-[85%] text-white">
             <h1 className="font-Poppins text-[14px] font-semibold">
-                Erro, verifique as informações cadastradas!
+                Erro, verifique as informações cadastradas!!
             </h1>
         </Alert>
     );
